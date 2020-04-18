39da65a56b27e2b2916228098015e0682167d4dd
Arunavo Ray <rayanup3@gmail.com>
2020-04-18 11:42:07 +0530

    Finally Calender layout and date selector issue fixed

--- static/js/graphs.js
+++ static/js/graphs.js
@@ -697,6 +697,27 @@ function makeGraphs(error, recordsJson) {
 		$('#inputdateend').attr('readonly', readonly).val(formatDate(end));
 	}
 
+	$('.form_date').datetimepicker({
+        // language:  'fr',
+        weekStart: 1,
+        todayBtn:  1,
+		autoclose: 1,
+		todayHighlight: 1,
+		startView: 2,
+		minView: 2,
+		forceParse: 0
+	});
+	$('.form_date').datetimepicker({
+        // language:  'fr',
+        weekStart: 1,
+        todayBtn:  1,
+		autoclose: 1,
+		todayHighlight: 1,
+		startView: 2,
+		minView: 2,
+		forceParse: 0
+    });
+
 	updateRange(minDate, maxDate);
 	
 	var latlng = L.latLng(23.07, 80.01);
